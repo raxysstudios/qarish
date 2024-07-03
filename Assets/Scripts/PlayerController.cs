aa1ebb69991a43697e904ce9a376095f4c28eb63
proGRAmist24578 <108571896+proGRAmist24578@users.noreply.github.com>
2024-07-03 17:31:24 +0300

    WASD controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements.Experimental;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _sprint;
+    private Vector2 _moveInput;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + _moveInput * _speed * Time.fixedDeltaTime);
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        _moveInput = inputValue.Get<Vector2>();
+    }
+
+    void OnSprint(InputValue inputValue){
+        print("player is run");
+    }
+
+
+}
