4151c8cb79ce017577ec23fb1f54ff6952b7ac0e
alkaitagi <alkaitagi@outlook.com>
2024-07-12 18:17:37 +0300

    Clean up

--- Assets/Scripts/SheepControl.cs
+++ Assets/Scripts/SheepControl.cs
@@ -1,40 +1,37 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
-
 public class SheepControl : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float walkSpeed = 5f;
     private Vector2? moveTarget;
     private bool isDirection = false;
-    
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
 
     }
-    
+
     private void FixedUpdate()
     {
         if (!moveTarget.HasValue)
         {
             return;
         }
-        if(!isDirection && (moveTarget.Value - rb.position).sqrMagnitude <= 1)
+        if (!isDirection && (moveTarget.Value - rb.position).sqrMagnitude <= 1)
         {
             moveTarget = null;
             return;
         }
-        var direction = isDirection ? moveTarget.Value: (moveTarget.Value - rb.position).normalized;
+        var direction = isDirection ? moveTarget.Value : (moveTarget.Value - rb.position).normalized;
         var move = walkSpeed * direction;
         rb.MovePosition(
             rb.position + move * Time.fixedDeltaTime
         );
-
     }
-    
+
     public void ReceiveCall(Vector2 point, bool isDirection = false)
     {
         this.isDirection = isDirection;
@@ -48,12 +45,10 @@ public class SheepControl : MonoBehaviour
             {
                 moveTarget = point;
             }
-            
         }
         else
         {
             moveTarget = null;
         }
     }
-    
 }
