d4000bac98d725c69193e2c11da18f74266d1991
alkaitagi <alkaitagi@outlook.com>
2024-08-07 13:15:05 +0300

    Implement basic flock queueing

--- Assets/Scripts/FlockController.cs
+++ Assets/Scripts/FlockController.cs
@@ -1,5 +1,5 @@
+using System.Collections;
 using System.Collections.Generic;
-using Pathfinding;
 using UnityEngine;
 
 public class FlockController : MonoBehaviour
@@ -10,34 +10,73 @@ public class FlockController : MonoBehaviour
     private Transform beacon;
 
     private Transform target;
+    public Transform Target
+    {
+        get => target;
+        set
+        {
+            target = value;
+            if (targetCoroutine != null)
+                StopCoroutine(targetCoroutine);
+            if (target != null)
+            {
+                targetCoroutine = UpdateTargets();
+                StartCoroutine(targetCoroutine);
+            }
+        }
+    }
 
+    [SerializeField]
+    private float followDelay = .5f;
+
+    IEnumerator targetCoroutine;
+    readonly List<Vector2> sheepPoints = new();
 
     public void Stop()
     {
-        target = null;
+        Target = null;
     }
 
     public void Move(Vector2 point)
     {
         beacon.position = point;
-        target = beacon;
+        Target = beacon;
+        Target = Target;
     }
 
     public void Follow(Transform target)
     {
-        this.target = target;
+        Target = target;
+        StartCoroutine(UpdateTargets());
+    }
+
+    IEnumerator UpdateTargets()
+    {
+        sheepPoints.Clear();
+        while (true)
+        {
+            sheep[0].ai.destination = target.position;
+            for (var i = 1; i < sheepPoints.Count; i++)
+                sheep[i].ai.destination = sheepPoints[i - 1];
+
+            if (sheepPoints.Count < sheep.Count)
+                sheepPoints.Add(Vector2.zero);
+            for (var i = 0; i < sheepPoints.Count; i++)
+                sheepPoints[i] = sheep[i].rb.position;
+
+            yield return new WaitForSeconds(followDelay);
+        }
     }
 
     void Update()
     {
-        if (target == null)
+        if (Target != null)
         {
+            if (beacon != Target)
+                beacon.position = Target.position;
+        }
+        else
             foreach (var s in sheep)
                 s.ai.destination = s.rb.position;
-            return;
-        }
-
-        foreach (var s in sheep)
-            s.ai.destination = target.position;
     }
 }
