63d4009559a10c309967f8a480fcf548b7bdc452
alkaitagi <alkaitagi@outlook.com>
2024-09-21 19:21:55 +0300

    Add smooth rotation for Orientation2D

--- Assets/Scripts/Orientation2D.cs
+++ Assets/Scripts/Orientation2D.cs
@@ -1,9 +1,9 @@
-using System;
 using UnityEngine;
 
 public class Orientation2D : MonoBehaviour
 {
     public Transform root;
+    public float smoothTime = .25f;
 
     public float Sign
     {
@@ -27,6 +27,23 @@ public class Orientation2D : MonoBehaviour
     public void TurnTo(Vector3 point)
     {
         Sign = point.x - transform.position.x;
+    }
+
+    float velocity = .0f;
+
+    public void LookAtSmooth(Vector3 point)
+    {
+        var angle = Vector2.SignedAngle(
+            Sign * Vector3.right,
+            point - root.position
+        );
+        angle = Mathf.SmoothDampAngle(
+            root.localEulerAngles.z,
+            angle,
+            ref velocity,
+            smoothTime
+        );
+        root.localEulerAngles = angle * Sign * Vector3.forward;
     }
 
     public void LookAt(Vector3 point)
